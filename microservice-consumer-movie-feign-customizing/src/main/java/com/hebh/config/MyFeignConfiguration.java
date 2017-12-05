1e1183f1009cd16ba2b4234ca2f573569969aa07
hebh <hebaohua@hust.edu.cn>
2017-12-05 21:23:06 +0800

    high available eureka

--- microservice-consumer-movie-feign-customizing/src/main/java/com/hebh/config/MyFeignConfiguration.java
+++ microservice-consumer-movie-feign-customizing/src/main/java/com/hebh/config/MyFeignConfiguration.java
@@ -3,7 +3,7 @@ package com.hebh.config;
 import feign.Contract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import feign.Logger;
 /**
  * @author Hebh
  * @date 2017/12/4
@@ -14,6 +14,11 @@ public class MyFeignConfiguration {
     @Bean
     public Contract feignContract() {
         return new feign.Contract.Default();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 
 }
